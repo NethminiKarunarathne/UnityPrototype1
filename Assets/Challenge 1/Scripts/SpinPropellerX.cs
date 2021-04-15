768df9a87c89208a493795add7e0b3ca9dab453f
Nethmini Karunarathne <nethminikarunarathne@gmail.com>
2021-04-15 20:47:43 +0530

    Add Unity Project 1

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,200* Time.deltaTime);
+    }
+}
